1ca4670ab61d8c13a6b253a619056f477abbfe6e
MounaGuelsa <mounaguelsa9991@gmail.com>
2024-02-18 15:43:49 +0100

    Initial commit

---
+++ src/main/java/com/example/mooney/MoonEyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonEyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonEyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mooney/MoonEyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mooney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonEyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
